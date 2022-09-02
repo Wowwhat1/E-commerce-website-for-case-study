2aae538dc5e9e96900a5cddf6650177be97c1ce3
Wowwhat1 <86320782+Wowwhat1@users.noreply.github.com>
2022-09-02 22:38:22 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+
+    <section class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+            <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+            <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+            <a href="login.php"> <i class="fas fa-angle-right"></i> Login</a>
+            <a href="register.php"> <i class="fas fa-angle-right"></i> Register</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> +84 905 019 506 </p>
+            <p> <i class="fas fa-phone"></i> +84 905 199 357 </p>
+            <p> <i class="fas fa-envelope"></i> chienhuynh234@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Da Nang, Viet Nam - 550000 </p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="https://www.facebook.com/congchien.huynh.7/" target="_blank"> <i class="fab fa-facebook-f"></i>
+                Facebook </a>
+            <a href="https://github.com/Wowwhat1" target="_blank"> <i class="fab fa-twitter"></i> Github </a>
+            <a href="https://www.linkedin.com/in/chi%E1%BA%BFn-hu%E1%BB%B3nh-3b0246234/" target="_blank"> <i
+                    class="fab fa-linkedin"></i> Linkedin </a>
+        </div>
+
+    </section>
+
+</footer>
